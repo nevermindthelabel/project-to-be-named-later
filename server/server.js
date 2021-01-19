542168c06562586dd226157944a1d2988a04a219
Matt Kilcup <nevermindthelabel@gmail.com>
2021-01-18 17:22:18 -0700

    updated back end

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes/index.js';
 
 const PORT = 3001 || process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => console.log(`server 💨 on port http://localhost:${PORT}`));
